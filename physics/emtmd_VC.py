8dc7061f9168a2dffb9b893009e23adc79cebcb9
Ilias Tsakonas <263033898+iliastsakonas@users.noreply.github.com>
2026-05-15 20:29:40 +0300

    Update emtmd_VC.py
    Remove unnecessary lines

--- physics/emtmd_VC.py
+++ physics/emtmd_VC.py
@@ -21,10 +21,10 @@ class EMTMDVCProblem(PhysicsProblem):
         self.w_range = range(500,12501,5)                                # Frequency range
         df = pd.read_csv('data/Mem.csv',header=None)
         self.Mem = torch.tensor(df.values, dtype=torch.complex64)         # Mass matrix of the electromechanical system (109x109): 
-        self.Mem = self.Mem                               # Inertia terms will be added later (Inductance)
+                                                                        # Inertia terms will be added later (Inductance)
         df = pd.read_csv('data/Cem.csv',header=None)
         self.Cem = torch.tensor(df.values, dtype=torch.complex64)         # Damping matrix of the electromechanical system (109x190)
-        self.Cem = self.Cem                            # Damping terms will be added later (Resistance)
+                                                                        # Damping terms will be added later (Resistance)
         df = pd.read_csv('data/Kem.csv',header=None)
         self.Kem = torch.tensor(df.values, dtype=torch.complex64)         # Stiffness matrix of the electromechanical system (118x118)
         self.Kem = self.Kem.unsqueeze(0)
@@ -63,11 +63,7 @@ class EMTMDVCProblem(PhysicsProblem):
         return output
 
     def compute_emtmd_response(self, predictions):
-        #start_time = time.perf_counter()
-        # Set pi for convenience
-        # predictions = torch.clamp(predictions, 1e-4, 1)
         p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18 = predictions[:, 0], predictions[:, 1], predictions[:, 2], predictions[:, 3], predictions[:, 4], predictions[:, 5], predictions[:, 6], predictions[:, 7], predictions[:, 8], predictions[:, 9], predictions[:, 10], predictions[:, 11], predictions[:, 12], predictions[:, 13], predictions[:, 14], predictions[:, 15], predictions[:, 16], predictions[:, 17]
-        #start_time = time.perf_counter()
         # Set up the system matrices
         Kem_l = self.Kem
         Force_l = self.Force_vec
@@ -75,20 +71,11 @@ class EMTMDVCProblem(PhysicsProblem):
         L_mat = (torch.diag((torch.cat( (p1, p2, p3, p4, p5, p6, p7, p8, p9)))))
         R_mat = (torch.diag((torch.cat( (p10, p11, p12, p13, p14, p15, p16, p17, p18)))))
         Cem_l = torch.cat((torch.cat((self.Cem, torch.zeros(109,9) ),1), torch.cat( (torch.zeros(9,109),R_mat) ,1 )),0).unsqueeze(0)
-        Mem_l = torch.cat((torch.cat((self.Mem, torch.zeros(109,9) ),1), torch.cat( (torch.zeros(9,109),L_mat) ,1 )),0).unsqueeze(0)
-        
+        Mem_l = torch.cat((torch.cat((self.Mem, torch.zeros(109,9) ),1), torch.cat( (torch.zeros(9,109),L_mat) ,1 )),0).unsqueeze(0)        
         DSM = Mem_l*self.s**2+Cem_l*self.s+Kem_l
         H = (torch.linalg.solve(DSM, Force_l))
         scale = 1e8
         output = torch.linalg.vector_norm(torch.abs(H[:,99,:]).squeeze(-1).squeeze(-1))*scale
-        #end_time = time.perf_counter()
-        #elapsed_time = end_time - start_time
-        #print(f"Code executed in {elapsed_time:.4f} seconds")
-        #output_np = H[:,99,:].detach().numpy()
-        #df = pd.DataFrame(output_np)
-        #df.to_csv("TestH.csv",index = False)
-        print(output)
-
         return output
 
     def constraint_loss(self, predictions):
