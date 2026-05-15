e0362cca44a68c790c5f71d9d52980f468fed8f1
Ilias Tsakonas <263033898+iliastsakonas@users.noreply.github.com>
2026-05-15 16:50:17 +0300

    Add files via upload

--- physics/emtmd_EH.py
+++ physics/emtmd_EH.py
@@ -1,4 +1,3 @@
-from re import L
 from physics.base import PhysicsProblem
 import torch
 import pandas as pd
@@ -7,32 +6,38 @@ import math
 
 
 class EMTMDEHProblem(PhysicsProblem):
+    """
+    EMTMD_EH inverse design problem.
+    Find the optimal values for the inductances (L) and resistances (R) of an electromechanical structure composed of
+    9 electromechanical TMDs attached in a clamped-free rod, to maximize the H2 norm of the energy harvesting efficiency
+    (electrical power generated / mechanical power input)
+    """
+
     def __init__(self):
         self._targets = None
         self._input_data = None
-        self.w_range = range(500,7501,5)                                # Frequency range
+        self.w_range = range(500,12501,5)                                # Frequency range
         df = pd.read_csv('data/Mem.csv',header=None)
-        self.Mem = torch.tensor(df.values, dtype=torch.float32)         # Mass matrix of the electromechanical system (109x109): 
-        self.Mem = self.Mem.unsqueeze(0).repeat(len(self.w_range),1,1)  # Inertia terms will be added later (Inductance
+        self.Mem = torch.tensor(df.values, dtype=torch.complex64)         # Mass matrix of the electromechanical system (109x109): 
+        self.Mem = self.Mem                               # Inertia terms will be added later (Inductance
         df = pd.read_csv('data/Cem.csv',header=None)
-        self.Cem = torch.tensor(df.values, dtype=torch.float32)         # Damping matrix of the electromechanical system (109x190)
-        self.Cem = self.Cem.unsqueeze(0).repeat(len(self.w_range),1,1)  # Damping terms will be added later (Resistance)
+        self.Cem = torch.tensor(df.values, dtype=torch.complex64)         # Damping matrix of the electromechanical system (109x190)
+        self.Cem = self.Cem                            # Damping terms will be added later (Resistance)
         df = pd.read_csv('data/Kem.csv',header=None)
-        self.Kem = torch.tensor(df.values, dtype=torch.float32)         # Stiffness matrix of the electromechanical system (118x118)
-        self.Kem = self.Kem.unsqueeze(0).repeat(len(self.w_range),1,1)
+        self.Kem = torch.tensor(df.values, dtype=torch.complex64)         # Stiffness matrix of the electromechanical system (118x118)
+        self.Kem = self.Kem.unsqueeze(0)
         self.Force_vec = np.zeros((118,1), dtype = np.complex64)        # Excitation force vector
         self.Force_vec[3,0] = 1                                      
-        self.Force_vec = torch.from_numpy(self.Force_vec).unsqueeze(0).repeat(len(self.w_range),1,1)
-        self.s = 1j*2*math.pi*torch.tensor(list(self.w_range),dtype = torch.float32).unsqueeze(-1).unsqueeze(-1)
-        
+        self.Force_vec = torch.from_numpy(self.Force_vec).unsqueeze(0)
+        self.s = 1j*2*math.pi*torch.tensor(list(self.w_range),dtype = torch.complex64).unsqueeze(-1).unsqueeze(-1)
         # Names for each design parameter: 1-9 values: Inductance (L), 10-18 values: Resistance (R)
         self.design_params = ['L1','L2','L3','L4','L5','L6','L7','L8','L9',
                               'R1','R2','R3','R4','R5','R6','R7','R8','R9'
         ]
 
         # Bounds for each design parameter
-        self.bounds = [(1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2), (1E-5, 3e-2),
-                       (1, 2500), (1, 2500), (1, 2500), (1, 2500), (1, 2500), (1, 2500), (1, 2500), (1, 2500), (1, 2500)
+        self.bounds = [(1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3), (1e-3, 0.3),
+                       (1, 300), (1, 300), (1, 300), (1, 300), (1, 300), (1, 300), (1, 300), (1, 300), (1, 300)
         ]
 
     def get_input_output_dims(self):
@@ -48,7 +53,7 @@ class EMTMDEHProblem(PhysicsProblem):
 
     def load_data(self, path):
         inp = torch.zeros((1,1))
-        obs = torch.ones((1,1))     # Maximum efficiency is set to 1
+        obs = 50*torch.ones((1,1))     # Maximum efficiency is set to 50 %
         return inp, obs
 
     def forward_physics(self, inputs, predictions):
@@ -56,32 +61,32 @@ class EMTMDEHProblem(PhysicsProblem):
         return output
 
     def compute_emtmdeh_response(self, predictions):
+        #start_time = time.perf_counter()
         # Set pi for convenience
         p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18 = predictions[:, 0], predictions[:, 1], predictions[:, 2], predictions[:, 3], predictions[:, 4], predictions[:, 5], predictions[:, 6], predictions[:, 7], predictions[:, 8], predictions[:, 9], predictions[:, 10], predictions[:, 11], predictions[:, 12], predictions[:, 13], predictions[:, 14], predictions[:, 15], predictions[:, 16], predictions[:, 17]
+        #start_time = time.perf_counter()
         # Set up the system matrices
-        # Clone the M, C, K matrices and F vector
-        Cem_new = self.Cem.clone()
-        Mem_new = self.Mem.clone()
-        Kem_l = self.Kem.clone()
-        Force_l = self.Force_vec.clone()
-        s_l = self.s.clone()
+        Kem_l = self.Kem
+        Force_l = self.Force_vec
         # Create the diagonal matrices for L and R values to be added to the Mem and Cem matrices
-        L_mat = (torch.diag(torch.cat( (p1, p2, p3, p4, p5, p6, p7, p8, p9)))).repeat(len(self.w_range),1,1)
-        R_mat = (torch.diag(torch.cat( (p10, p11, p12, p13, p14, p15, p16, p17, p18)))).repeat(len(self.w_range),1,1)
-        Cem_l = torch.cat((torch.cat((Cem_new, torch.zeros(len(self.w_range),109,9) ),2), torch.cat( (torch.zeros(len(self.w_range),9,109),R_mat) ,2 )),1)
-        Mem_l = torch.cat((torch.cat((Mem_new, torch.zeros(len(self.w_range),109,9) ),2), torch.cat( (torch.zeros(len(self.w_range),9,109),L_mat) ,2 )),1)
-        # Dynamic Stiffness Matrix
-        DSM = (Mem_l*s_l**2+Cem_l*s_l+Kem_l)
-        # Solve the linear system
-        H = torch.linalg.solve(DSM, Force_l)
+        L_mat = (torch.diag(torch.cat( (p1, p2, p3, p4, p5, p6, p7, p8, p9))))
+        R_mat = (torch.diag(torch.cat( (p10, p11, p12, p13, p14, p15, p16, p17, p18))))
+        Cem_l = torch.cat((torch.cat((self.Cem, torch.zeros(109,9) ),1), torch.cat( (torch.zeros(9,109),R_mat) ,1 )),0).unsqueeze(0)
+        Mem_l = torch.cat((torch.cat((self.Mem, torch.zeros(109,9) ),1), torch.cat( (torch.zeros(9,109),L_mat) ,1 )),0).unsqueeze(0)
+        DSM = Mem_l*self.s**2+Cem_l*self.s+Kem_l
+        H = (torch.linalg.solve(DSM, Force_l))
         # RMS of the Generated Power in the Transducers (I^2*R/2)
-        genPower = torch.bmm(s_l**2,torch.bmm((H[:,-9:,:]).transpose(1,2), torch.bmm(R_mat.to(dtype=torch.complex64), (H[:,-9:,:]))))/2
+        #genPower = torch.bmm(s_l**2,torch.bmm((H[:,-9:,:]).bT, torch.bmm(R_mat.to(dtype=torch.complex64), (H[:,-9:,:]))))/2
+        genPower = (self.s**2*H[:,-9:,:].mH@R_mat.to(dtype = torch.complex64)@H[:,-9:,:]/2).squeeze(-1).squeeze(-1)
         # Input Mechanical Power (Force*Velocity)
-        inPower = torch.bmm(s_l,torch.matmul(H.transpose(1,2),Force_l))
-        # Measurement of the conversion of mechanical power to electrical power
-        # efficiency = torch.div(genPower,inPower)
-        # Output: The efficiency of the system to convert the mechanical power into electrical, over a frequency band
-        output = torch.linalg.vector_norm(torch.abs(torch.div(genPower,inPower)).squeeze(-1).squeeze(-1))/math.sqrt(len(self.w_range))
+        inPower = (self.s*H.mH@Force_l).squeeze(-1).squeeze(-1)
+        # Measurement of the conversion of mechanical power to electrical power: Energy Harvesting Efficiency = genPower / inPower
+        # Output: Banded H2 norm of the energy harvesting efficiency
+        #output_np = torch.abs(torch.div(genPower,inPower)*100).detach().numpy()
+        #df = pd.DataFrame(output_np)
+        #df.to_csv("TestH.csv",index = False)
+        output = torch.linalg.vector_norm(torch.abs(torch.div(genPower,inPower)*100)/math.sqrt(self.s.size()[0]))
+        print(output)
         return output
 
     def constraint_loss(self, predictions):
@@ -93,7 +98,7 @@ class EMTMDEHProblem(PhysicsProblem):
 
         if epoch_results is not None:
             epoch_csv_path = os.path.join(output_dir, 'epoch_results.csv')
-            save_emtmd_epoch_results_csv(epoch_results, output_dir)
+            save_emtmd_epoch_results_csv(epoch_results, output_dir,history)
 
         plot_loss_curves(history, output_dir)
 
